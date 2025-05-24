60c4bacd678d95ba1853f172319c61d69d568144
David Arabuli <davidsarabuli@gmail.com>
2025-05-24 11:11:25 +0300

    viewport meta tag

--- views/index.php
+++ views/index.php
@@ -9,6 +9,7 @@ unset($_SESSION['errors'], $_SESSION['old']);
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Unit Editor</title>
     <link rel="stylesheet" href="/css/style.css">
 
